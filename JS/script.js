66795b64666fd85b48c4c5649991f00c100c9492
Ksenia <example@gmail.com>
2023-10-29 12:50:04 -0400

    first commit

---
+++ JS/script.js
@@ -0,0 +1,82 @@
+"use strict";
+
+// alert('Hello');
+
+// let number = 5
+// const leftBorderWidth = 1;
+// const COLOR_RED = '#F00';
+// const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+
+// let userName = "John";
+// let userNumber = 25;
+// userNumber = 24;
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// number = 10;
+// console.log(number);
+
+// let number = 4.6;
+
+// console.log('string'*9);
+
+// const bool = true;
+
+// console.log(something);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+//
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 6, 'apple.bmp'];
+// console.log(arr[2]);
+// console.log(arr['plum.png']);
+
+// let arr = ['a', 'b', 'c'];
+// console.log(arr[1]);
+// let obj = {0: 'a', 1: 'b', 2: 'c'};
+// console.log(obj[1]);
+// obj.d = '1234';
+// console.log(obj['d']);
+
+// const results = confirm('Are you here?');
+// console.log(results);
+// console.log(typeof(results));
+
+// const answer = +prompt("are you 18?","18");
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt('what is your given name?', '')
+// answers[1] = prompt('what is your family name?', '')
+// answers[2] = prompt('what is your age?', '')
+// document.write(answers);
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}`);
+
+// const user = 'Ivan';
+// alert(`Hi, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+//
+// incr++;
+// decr--;
+//
+// ++incr;
+// --decr;
+
+// const isChecked = true,
+//     isClosed = false;
+// console.log(isChecked || isClosed);
