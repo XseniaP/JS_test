17fed6074cb81d1b3fa1e35723448748480d7ce2
Ksenia <example@gmail.com>
2023-10-31 17:45:08 -0400

    movie DB created

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,79 @@
 "use strict";
 
+// const lines = 5;
+// let result = '';
+// let space = " ";
+// let star = "*";
+//
+// for (let i = 1, row = lines; i < 12; i+=2, row--){
+//     result = result + space.repeat(row) + star.repeat(i) + "\n";
+// }
+// console.log(result);
+
+// const lines = 5;
+// let result = '';
+// // Проверяется именно переменная result, формируйте строку в ней
+// let space = ' ';
+// let star = '*';
+//
+// for (let i = 1, row = 4; i < 10; i+=2, row--){
+//     result += row*space + star*i + "\n";
+// }
+// console.log(result);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+//
+// // Пишем решение вот тут
+// for (let i = 0; i < data.length; i++) {
+//     if (typeof (data[i]) === "string") {
+//         data[i] += " - done";
+//     } else if (typeof (data[i]) === "number") {
+//         data[i] = data[i] * 2;
+//     }
+// }
+// console.log(data);
+
+
+
+// let num = 50;
+
+// for (let i=5; i<11; i++){
+//     console.log(i);
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     if (i === 6) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
+
 let numberOfFilms = +prompt("How many movies have you watched so far?",'');
+if (numberOfFilms < 10){
+    alert('you watched a few movies');
+} else if ((numberOfFilms>=10) && (numberOfFilms < 30)){
+    alert('you watched a lot of movies');
+} else if (numberOfFilms>=30) {
+    alert('you are movie-lover');
+} else {
+    alert('Error');
+}
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,16 +83,67 @@ let personalMovieDB = {
     privat: false
 };
 
-let lastMovie = prompt("Name one of the last movies you've watched?",''), movieRank = +prompt("How would you rate that movie?",'');
-personalMovieDB.movies[lastMovie] = movieRank;
 
-lastMovie = prompt("Name one of the last movies you've watched?",'');
-movieRank = +prompt("How would you rate that movie?",'');
-personalMovieDB.movies[lastMovie] = movieRank;
-
+let lastMovie, movieRank;
+for (let i=0; i < 2; i++){
+    lastMovie = prompt("Name one of the last movies you've watched?",'');
+    movieRank = +prompt("How would you rate that movie?",'');
+    if (lastMovie != '' && lastMovie != null && movieRank != '' && movieRank != null && lastMovie.length < 50){
+        personalMovieDB.movies[lastMovie] = movieRank;
+    } else {
+        console.log('error');
+        i--; //return 1 cycle back
+    }
+}
 console.log(personalMovieDB);
 
+// console.log(0 || 0 || null);
 
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+//
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets){
+//     console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+//     console.log('everyone is happy');
+// } else {
+//     console.log(' we are leaving')
+// }
+// const num = 50;
+// (num == 50) ? console.log('ok!') : console.log('Error');
+
+// switch (num) {
+//     case 49:
+//         console.log('wrong');
+//         break;
+//     case 100:
+//         console.log('wrong');
+//         break;
+//     case 50:
+//         console.log('true');
+//         break;
+//     default:
+//         console.log('not this time');
+//         break;
+// }
+
+// if (4 == 4) {
+//     console.log('ok!');
+// } else {
+//     console.log('Error');
+// }
+//
+// if (1) {
+//     console.log('ok!');
+// } else if (1) {
+//     console.log('Error');
+// } else {
+//         console.log('Error');
+// }
 // alert('Hello');
 
 //works
